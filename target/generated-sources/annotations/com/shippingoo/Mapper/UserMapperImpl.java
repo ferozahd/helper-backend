8f50ca21d3991b048c06b9e476bdaec4fed0ad9f
unknown <feroz.shah.940@gmail.com>
2022-03-04 19:46:26 +0600

    deleting html and css

--- target/generated-sources/annotations/com/shippingoo/Mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/shippingoo/Mapper/UserMapperImpl.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-03-03T23:11:29+0600",
-    comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 1.4.50.v20210914-1429, environment: Java 17.0.1 (Eclipse Adoptium)"
+    date = "2022-03-04T19:44:47+0600",
+    comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 1.4.50.v20210914-1429, environment: Java 17.0.2 (Eclipse Adoptium)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
